e022ffe245391efa9ac9803ad6b56c0cbf158ad0
Moritz Kleine <mkleine@coremedia.com>
2024-10-02 15:21:50 +0200

    use a single blob service
    avoid memory leak due to cleanup thread started by each TempFileFactory instance
    
    it would be preferable to use com.coremedia.cms.common.plugins.beans_for_plugins2.CommonBeansForPluginsConfiguration and not to create a fallback blob service, but that's currently prevented by com.coremedia.contenthub.api.BaseFileSystemConfiguration

--- studio-server/src/main/java/com/coremedia/labs/plugins/adapters/cloudinary/CloudinaryConfiguration.java
+++ studio-server/src/main/java/com/coremedia/labs/plugins/adapters/cloudinary/CloudinaryConfiguration.java
@@ -1,19 +1,37 @@
 package com.coremedia.labs.plugins.adapters.cloudinary;
 
+import com.coremedia.cap.common.BlobService;
+import com.coremedia.cap.common.CapConnection;
+import com.coremedia.cms.common.plugins.beans_for_plugins_container.CommonBeansForPluginsContainer;
 import com.coremedia.contenthub.api.BaseFileSystemConfiguration;
 import com.coremedia.contenthub.api.ContentHubAdapterFactory;
 import com.coremedia.contenthub.api.ContentHubMimeTypeService;
 import com.coremedia.contenthub.api.ContentHubType;
+import com.coremedia.cotopaxi.common.blobs.BlobServiceImpl;
+import com.coremedia.mimetype.MimeTypeService;
+import com.coremedia.util.TempFileFactory;
 import edu.umd.cs.findbugs.annotations.NonNull;
+import edu.umd.cs.findbugs.annotations.Nullable;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 
 import java.util.Map;
 
+import static java.lang.invoke.MethodHandles.lookup;
+
 @Configuration
 @Import({BaseFileSystemConfiguration.class})
-public class CloudinaryConfiguration {
+public class CloudinaryConfiguration implements DisposableBean {
+
+  private static final Logger LOG = LoggerFactory.getLogger(lookup().lookupClass());
+
+  @Nullable
+  private TempFileFactory tempFileFactory;
 
   private static Map<ContentHubType, String> typeMapping() {
     return Map.of(
@@ -30,7 +48,31 @@ public class CloudinaryConfiguration {
   }
 
   @Bean
-  public ContentHubAdapterFactory<?> cloudinaryContentHubAdapterFactory(@NonNull ContentHubMimeTypeService mimeTypeService) {
-    return new CloudinaryContentHubAdapterFactory(mimeTypeService, typeMapping());
+  public ContentHubAdapterFactory<?> cloudinaryContentHubAdapterFactory(@NonNull ContentHubMimeTypeService mimeTypeService,
+                                                                        BlobService blobService) {
+    return new CloudinaryContentHubAdapterFactory(mimeTypeService, blobService, typeMapping());
+  }
+
+  @Bean
+  public BlobService contentHubMimeTypeService(ObjectProvider<CommonBeansForPluginsContainer> commonBeansForPluginsContainer,
+                                               MimeTypeService mimeTypeService) {
+    return commonBeansForPluginsContainer.stream()
+            .map(CommonBeansForPluginsContainer::getConnection)
+            .map(CapConnection::getBlobService)
+            .findFirst()
+            .orElseGet(() -> createBlobService(mimeTypeService));
+  }
+
+  private BlobService createBlobService(MimeTypeService mimeTypeService) {
+    LOG.warn("Creating fallback BlobService for Cloudinary adapter because CapConnection is not available. Please check your plugin configuration.");
+    tempFileFactory = new TempFileFactory();
+    return new BlobServiceImpl(tempFileFactory, mimeTypeService);
+  }
+
+  @Override
+  public void destroy() {
+    if (tempFileFactory != null) {
+      tempFileFactory.destroy();
+    }
   }
 }
